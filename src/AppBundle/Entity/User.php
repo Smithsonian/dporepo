3f0677c595e5ceee88d02e6732f055e0fa044726
Goran Halusa <ghalusa@gmail.com>
2017-12-21 21:59:34 -0500

    Added Active Directory auth support

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,27 +1,45 @@
 <?php
-// src/AppBundle/Entity/User.php
-
+// src/AppBundle/Entity/FOSUser.php
 namespace AppBundle\Entity;
-
+ 
 use FOS\UserBundle\Model\User as BaseUser;
+use FR3D\LdapBundle\Model\LdapUserInterface;
 use Doctrine\ORM\Mapping as ORM;
-
+ 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser
-{
+class User extends BaseUser implements LdapUserInterface{
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-
+ 
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $dn;
+ 
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+        if (empty($this->roles)) {
+           $this->roles[] = 'ROLE_USER';
+        }
     }
+ 
+    /**
+     * {@inheritDoc}
+     */
+    public function setDn($dn)
+    { $this->dn = $dn; }
+ 
+    /**
+     * {@inheritDoc}
+     */
+    public function getDn()
+    { return $this->dn; }
 }
