17928b38d29e1b7cbe2dabd27570497040c9ca71
Goran Halusa <ghalusa@gmail.com>
2017-12-25 18:52:34 -0500

    Added base-level Resources files

---
+++ src/AppBundle/Controller/ResourcesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\DBAL\Driver\Connection;
+
+use PDO;
+use GUMP;
+
+// Custom utility bundles
+use AppBundle\Utils\GumpParseErrors;
+use AppBundle\Utils\AppUtilities;
+
+class ResourcesController extends Controller
+{
+    /**
+     * @var object $u
+     */
+    public $u;
+
+    /**
+    * Constructor
+    * @param object  $u  Utility functions object
+    */
+    public function __construct(AppUtilities $u)
+    {
+        $this->u = $u;
+    }
+
+    /**
+     * @Route("/admin/resources/", name="resources_home", methods="GET")
+     */
+    public function show_admin(Connection $conn, Request $request)
+    {
+        return $this->render('resources/resources.html.twig', array(
+            'page_title' => 'Resources',
+        ));
+    }
+
+}
