7467c66314bf17ee4509773c9dcd3b58c1a15114
Goran Halusa <ghalusa@gmail.com>
2018-01-15 13:04:09 -0500

    Implemented resizable columns

--- web/lib/javascripts/javascript.js
+++ web/lib/javascripts/javascript.js
@@ -15,7 +15,9 @@ $(document).ready(function(){
     $("#main_side_nav li.resources").show();
   }
 
-  // Set/remove favorites
+  /**
+   * Set/Remove Favorites
+   */
   $('.custom-checkbox .glyphicon').on('click', function(e) {
 
     const favoritePath = '/' + currentPath.join('/');
@@ -69,6 +71,82 @@ $(document).ready(function(){
 
     }, 500);
 
+  });
+
+  /**
+   * Resizable Columns
+   */
+  $(function() {
+    var resizableEl = $('.resizable').not(':last-child'),
+        columns = 12,
+        fullWidth = resizableEl.parent().width(),
+        columnWidth = fullWidth / columns,
+        totalCol, // This is filled by start event handler.
+    updateClass = function(el, col) {
+      el.css('width', ''); // Remove width, our class already has it.
+      // Set the offset value.
+      var offsetClassValue = '';
+      if(el.hasClass('main')) {
+        offsetClassValue = (col <= 8) ? ' col-sm-offset-4' : ' col-sm-offset-3';
+        offsetClassValue = (col <= 7) ? ' col-sm-offset-5' : offsetClassValue;
+        offsetClassValue = (col === 9) ? ' col-sm-offset-3' : offsetClassValue;
+        offsetClassValue = (col > 9) ? ' col-sm-offset-2' : offsetClassValue;
+      }
+      el.removeClass(function(index, className) {
+        return (className.match(/(^|\s)col-\S+/g) || []).join(' ');
+      }).addClass('col-sm-' + col + offsetClassValue);
+    };
+    // jQuery UI Resizable
+    resizableEl.resizable({
+      handles: 'e',
+      start: function(event, ui) {
+        var
+          target = ui.element,
+          next = target.next(),
+          targetCol = Math.round(target.width() / columnWidth),
+          nextCol = Math.round(next.width() / columnWidth);
+        // Set totalColumns globally.
+        totalCol = targetCol + nextCol;
+        target.resizable('option', 'minWidth', columnWidth);
+        target.resizable('option', 'maxWidth', 560);
+      },
+      resize: function(event, ui) {
+        var target = ui.element,
+            next = target.next(),
+            targetColumnCount = Math.round(target.width() / columnWidth),
+            nextColumnCount = Math.round(next.width() / columnWidth),
+            targetSet = totalCol - nextColumnCount,
+            nextSet = totalCol - targetColumnCount;
+
+        updateClass(target, targetSet);
+        updateClass(next, nextSet);
+      },
+    });
+  });
+
+  /**
+   * Left Navigation
+   */
+  $("#main_side_nav li.nav-header").on("click",function(){
+    $(this).nextUntil("li.nav-header").slideToggle('fast');
+    $(this).find("i").toggleClass("glyphicon glyphicon-chevron-down");
+    $(this).find("i").toggleClass("glyphicon glyphicon-chevron-right");
+  });
+  $("#hide_side_nav").on('click',function(){
+    var nav_bar = $(this).closest(".sidebar-nav");
+    nav_bar.removeClass("col-sm-3, col-md-2").hide();
+    var content_bar = nav_bar.next("div");
+    content_bar.removeClass("col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2");
+    content_bar.addClass("col-sm-12 col-md-12");
+    $("#show_side_nav").show();
+  });
+  $("#show_side_nav").on('click',function(){
+    var nav_bar = $("#hide_side_nav").closest(".sidebar-nav");
+    nav_bar.addClass("col-sm-3, col-md-2").show();
+    var content_bar = nav_bar.next("div");
+    content_bar.removeClass("col-sm-12 col-md-12");
+    content_bar.addClass("col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2");
+    $("#show_side_nav").hide();
   });
 
 });
