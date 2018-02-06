dc39fb5c00a75f7524594aa3e9ebb00342d72628
Goran Halusa <ghalusa@gmail.com>
2018-02-06 11:10:11 -0500

    Added dev1's IP address to trusted proxies

--- web/app.php
+++ web/app.php
@@ -16,6 +16,23 @@ if (PHP_VERSION_ID < 70000) {
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+
+// tell Symfony about your reverse proxy
+Request::setTrustedProxies(
+    // the IP address (or range) of your proxy
+    // ['192.0.0.1', '10.0.0.0/8'],
+    ['172.16.26.11'],
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_X_FORWARDED_ALL
+
+    // or, if your proxy instead uses the "Forwarded" header
+    // Request::HEADER_FORWARDED
+
+    // or, if you're using AWS ELB
+    // Request::HEADER_X_FORWARDED_AWS_ELB
+);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
