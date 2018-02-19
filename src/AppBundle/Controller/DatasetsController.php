9c84669347f2f1cae01dbe027dd7fd62c0637e80
Goran Halusa <ghalusa@gmail.com>
2018-02-19 14:07:01 -0500

    Mapped lookups for controlled vocabularies in Datasets Browse (DPO3DREP-54)

--- src/AppBundle/Controller/DatasetsController.php
+++ src/AppBundle/Controller/DatasetsController.php
@@ -164,7 +164,6 @@ class DatasetsController extends Controller
         $statement = $conn->prepare("SELECT SQL_CALC_FOUND_ROWS
               datasets.datasets_id AS manage
               ,datasets.dataset_guid
-              ,datasets.capture_method_lookup_id
               ,datasets.dataset_type_lookup_id
               ,datasets.dataset_name
               ,datasets.collected_by
@@ -172,20 +171,30 @@ class DatasetsController extends Controller
               ,datasets.date_of_capture
               ,datasets.dataset_description
               ,datasets.dataset_collection_notes
-              ,datasets.item_position_type_lookup_id
               ,datasets.positionally_matched_sets_id
               ,datasets.motion_control
-              ,datasets.focus_lookup_id
               ,datasets.light_source
-              ,datasets.light_source_type_lookup_id
               ,datasets.scale_bars_used
-              ,datasets.background_removal_method_lookup_id
-              ,datasets.camera_cluster_type_lookup_id
               ,datasets.array_geometry_id
               ,datasets.date_created
               ,datasets.last_modified
               ,datasets.datasets_id AS DT_RowId
+              ,capture_methods.label AS capture_method
+              ,dataset_types.label AS dataset_type
+              ,item_position_types.label_alias AS item_position_type
+              ,focus_types.label AS focus_type
+              ,light_source_types.label AS light_source_type
+              ,background_removal_methods.label AS background_removal_method
+              ,camera_cluster_types.label AS camera_cluster_type
           FROM datasets
+          LEFT JOIN capture_methods ON capture_methods.capture_methods_id = datasets.capture_method_lookup_id
+          LEFT JOIN dataset_types ON dataset_types.dataset_types_id = datasets.dataset_type_lookup_id
+          LEFT JOIN item_position_types ON item_position_types.item_position_types_id = datasets.item_position_type_lookup_id
+          LEFT JOIN focus_types ON focus_types.focus_types_id = datasets.focus_lookup_id
+          LEFT JOIN light_source_types ON light_source_types.light_source_types_id = datasets.light_source_type_lookup_id
+          LEFT JOIN background_removal_methods ON background_removal_methods.background_removal_methods_id = datasets.background_removal_method_lookup_id
+          LEFT JOIN camera_cluster_types ON camera_cluster_types.camera_cluster_types_id = datasets.camera_cluster_type_lookup_id
+          
           WHERE datasets.active = 1
           AND items_id = " . (int)$items_id . "
           {$search_sql}
