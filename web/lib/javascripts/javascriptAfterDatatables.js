f9e36f2e0b7785639f3cc56fdf0b7cffc8479c75
Goran Halusa <ghalusa@gmail.com>
2018-02-22 15:52:53 -0500

    Removed delete button from tables in Dashboard

--- web/lib/javascripts/javascriptAfterDatatables.js
+++ web/lib/javascripts/javascriptAfterDatatables.js
@@ -1,11 +1,17 @@
 jQuery(document).ready(function($) {
 
-  var remove_button = $('<a />')
-        .addClass('btn btn-default btn-sm')
-        .attr('id', 'remove-records-button')
-        .attr('href', 'javascript:void(0);')
-        .html('<span class="glyphicon glyphicon-trash"></span> Remove Records');
+	var urlSplit = window.location.href.split('/');
+	var currentPath = urlSplit.slice(3);
 
-  $('.datatables_bulk_actions').prepend(remove_button);
+	if(currentPath[1].length) {
+
+	  var remove_button = $('<button></button>')
+	        .addClass('btn btn-default glyphicon glyphicon-trash')
+	        .attr('style', 'width: 12rem;')
+	        .attr('id', 'remove-records-button');
+
+	  $('.datatables_bulk_actions').prepend(remove_button);
+
+	}
 
 });
