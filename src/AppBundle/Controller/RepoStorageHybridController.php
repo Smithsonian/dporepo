7890d598d6d8fef09650cc4cd813fd536632b0ad
rbartlettquotient <rbartlett@quotient-inc.com>
2018-03-19 17:58:02 -0400

    Add build function to controller, allowing controllers to call repo storage structure service.

--- src/AppBundle/Controller/RepoStorageHybridController.php
+++ src/AppBundle/Controller/RepoStorageHybridController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Service\RepoStorageHybrid;
+use AppBundle\Service\RepoStorageStructureHybrid;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\Container;
 use Doctrine\DBAL\Driver\Connection;
@@ -11,6 +12,7 @@ class RepoStorageHybridController extends Controller
 {
 
   private $repo_storage;
+  private $repo_storage_structure;
 
   public function __construct() {
     //$this->container = $this->container->get('doctrine.dbal.default_connection');
@@ -26,6 +28,20 @@ class RepoStorageHybridController extends Controller
     }
     else {
       return $this->repo_storage->$function($parameters);
+    }
+
+  }
+
+  public function build($function, $parameters) {
+    $this->connection = $this->container->get('doctrine.dbal.default_connection');
+    $this->repo_storage_structure = new RepoStorageStructureHybrid($this->connection);
+
+    if(!method_exists($this->repo_storage_structure, $function)) {
+      //@todo
+      return NULL;
+    }
+    else {
+      return $this->repo_storage_structure->$function($parameters);
     }
 
   }
