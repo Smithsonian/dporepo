20993a1df68800103ab860c8e333e2895f3697e2
rbartlettquotient <rbartlett@quotient-inc.com>
2018-03-26 13:06:49 -0400

    Added strict validation for integer fields (DPO3DREP-183)
    Gor commit 456bbc015943259105e8a727a6df9aa112cf866c

--- src/AppBundle/Entity/DatasetElements.php
+++ src/AppBundle/Entity/DatasetElements.php
@@ -10,30 +10,55 @@ class DatasetElements
 
     /**
      * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
+     * @Assert\Length(min="1", max="10")
      * @var int
      */
     public $capture_device_configuration_id;
 
     /**
      * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
+     * @Assert\Length(min="1", max="10")
      * @var int
      */
     public $capture_device_field_id;
 
     /**
      * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
+     * @Assert\Length(min="1", max="10")
      * @var int
      */
     public $capture_sequence_number;
 
     /**
      * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
+     * @Assert\Length(min="1", max="10")
      * @var int
      */
     public $cluster_position_field_id;
 
     /**
      * @Assert\NotBlank()
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
+     * @Assert\Length(min="1", max="10")
      * @var int
      */
     public $position_in_cluster_field_id;
