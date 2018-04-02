1d8125f9f6b44873c4563709cba56747c0f71912
Goran Halusa <ghalusa@gmail.com>
2018-04-02 18:28:04 -0400

    Temporary options set for several select inputs

--- src/AppBundle/Form/UvMapForm.php
+++ src/AppBundle/Form/UvMapForm.php
@@ -4,19 +4,30 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class UvMapForm extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
+
+    $data = (array)$options['data'];
+
     $builder
       ->add('parent_capture_dataset_repository_id', HiddenType::class, array(
         'required' => true,
       ))
-      ->add('map_type', null, array(
-        'label' => 'Map Type',
-        'required' => true,
-      ))
+      // TODO: hook-up to JSON schema
+      ->add('map_type', ChoiceType::class, array(
+          'label' => 'Map Type',
+          'required' => true,
+          'placeholder' => 'Select',
+          // All options
+          'choices' => array('normal' => 1, 'ambient occlusion' => 2, 'photo texture' => 3, 'hole fill' => 4),
+          // Selected option
+          'data' => $data['map_type'],
+          'attr' => array('class' => 'default-chosen-select'),
+        ))
       ->add('map_file_type', null, array(
         'label' => 'Map File Type',
         'required' => false,
