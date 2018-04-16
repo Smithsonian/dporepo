a1a6f359188367a603c9a7c125bc7258fe2368e1
Goran Halusa <ghalusa@gmail.com>
2018-04-16 17:46:43 -0400

    Finished import validation page logic and template (DPO3DREP-179)

--- src/AppBundle/Controller/ValidateMetadataController.php
+++ src/AppBundle/Controller/ValidateMetadataController.php
@@ -38,6 +38,84 @@ class ValidateMetadataController extends Controller
     }
 
     /**
+     * @param string $uploads_directory The upload directory
+     * @return array Import result and/or any messages
+     */
+    public function construct_import_data($uploads_directory = null)
+    {
+
+      $json_object = array();
+
+      if(!empty($uploads_directory)) {
+
+        $finder = new Finder();
+        $finder->files()->in($uploads_directory);
+
+        // Assign keys to each CSV, with projects first, subjects second, and items third.
+        foreach ($finder as $file) {
+          if(stristr($file->getRealPath(), 'projects')) {
+            $csv[0] = $file->getContents();
+          }
+          if(stristr($file->getRealPath(), 'subjects')) {
+             $csv[1] = $file->getContents();
+          }
+          if(stristr($file->getRealPath(), 'items')) {
+             $csv[2] = $file->getContents();
+          }
+        }
+
+        // Sort the CSV array by key.
+        ksort($csv);
+
+        foreach ($csv as $csv_key => $csv_value) {
+
+          // Convert the CSV to JSON.
+          $array = array_map('str_getcsv', explode("\n", $csv_value));
+          $json = json_encode($array);
+
+          // Convert the JSON to a PHP array.
+          $json_array = json_decode($json, false);
+
+          // Read the first key from the array, which is the column headers.
+          $target_fields = $json_array[0];
+
+          // TODO: move into a vz-specific method?
+          // [VZ IMPORT ONLY] Convert field names to satisfy the validator.
+          foreach ($target_fields as $tfk => $tfv) {
+            // [VZ IMPORT ONLY] Convert the 'import_subject_id' field name to 'subject_repository_id'.
+            if($tfv === 'import_subject_id') {
+              $target_fields[$tfk] = 'subject_repository_id';
+            }
+          }
+
+          // Remove the column headers from the array.
+          array_shift($json_array);
+
+          foreach ($json_array as $key => $value) {
+            // Replace numeric keys with field names.
+            foreach ($value as $k => $v) {
+              $field_name = $target_fields[$k];
+              unset($json_array[$key][$k]);
+              // If present, bring the project_repository_id into the array.
+              $json_array[$key][$field_name] = ($field_name === 'project_repository_id') ? (int)$id : null;
+              // TODO: move into a vz-specific method?
+              // [VZ IMPORT ONLY] Strip 'USNM ' from the 'subject_repository_id' field.
+              $json_array[$key][$field_name] = ($field_name === 'subject_repository_id') ? (int)str_replace('USNM ', '', $v) : $v;
+            }
+            // Convert the array to an object.
+            $json_object[$csv_key][] = (object)$json_array[$key];
+          }
+
+        }
+
+      }
+
+      // $this->u->dumper($json_object);
+
+      return $json_object;
+    }
+
+    /**
      * Matches /admin/projects/validate_metadata/*
      *
      * Route_DISABLED("/admin/projects/validate_metadata/{id}", name="validate_metadata", methods={"GET"}, defaults={"id" = null})
@@ -51,7 +129,7 @@ class ValidateMetadataController extends Controller
 
       // $this->u->dumper($id);
 
-      $blacklisted_fields = array();
+      $blacklisted_fields = $csv_data = $json_validation_result = array();
 
       // TODO: feed this into this method.
       if(empty($id)) {
@@ -61,94 +139,33 @@ class ValidateMetadataController extends Controller
       }
 
       $uploads_directory = __DIR__ . '/../../../web/uploads/';
+      $csv_data = $this->construct_import_data($uploads_directory);
 
-      $finder = new Finder();
-      $finder->files()->in($uploads_directory);
+      if(!empty($csv_data)) {
 
-      // Assign keys to each CSV, with projects first, subjects second, and items third.
-      foreach ($finder as $file) {
-        if(stristr($file->getRealPath(), 'projects')) {
-          $csv[0] = $file->getContents();
-        }
-        if(stristr($file->getRealPath(), 'subjects')) {
-           $csv[1] = $file->getContents();
-        }
-        if(stristr($file->getRealPath(), 'items')) {
-           $csv[2] = $file->getContents();
-        }
-      }
-
-      // Sort the CSV array by key.
-      ksort($csv);
-
-      $json_validation_result = array();
-
-      foreach ($csv as $csv_key => $csv_value) {
-
-        // Convert the CSV to JSON.
-        $array = array_map('str_getcsv', explode("\n", $csv_value));
-        $json = json_encode($array);
-
-        // Convert the JSON to a PHP array.
-        $json_array = json_decode($json, false);
-
-        // Read the first key from the array, which is the column headers.
-        $target_fields = $json_array[0];
-
-        // TODO: move into a vz-specific method?
-        // [VZ IMPORT ONLY] Convert field names to satisfy the validator.
-        foreach ($target_fields as $tfk => $tfv) {
-          // [VZ IMPORT ONLY] Convert the 'import_subject_id' field name to 'subject_repository_id'.
-          if($tfv === 'import_subject_id') {
-            $target_fields[$tfk] = 'subject_repository_id';
+        foreach ($csv_data as $csv_key => $csv_value) {
+          // Projects
+          if($csv_key === 0) {
+            // $json_validation_result[$csv_key] = (object)$this->repoValidate->validateData($csv_data[$csv_key], 'project', $blacklisted_fields);
+            // $this->u->dumper($json_validation_result);
           }
-        }
-
-        // Remove the column headers from the array.
-        array_shift($json_array);
-
-        foreach ($json_array as $key => $value) {
-          // Replace numeric keys with field names.
-          foreach ($value as $k => $v) {
-            $field_name = $target_fields[$k];
-            unset($json_array[$key][$k]);
-            // If present, bring the project_repository_id into the array.
-            $json_array[$key][$field_name] = ($field_name === 'project_repository_id') ? (int)$id : null;
-            // TODO: move into a vz-specific method?
-            // [VZ IMPORT ONLY] Strip 'USNM ' from the 'subject_repository_id' field.
-            $json_array[$key][$field_name] = ($field_name === 'subject_repository_id') ? (int)str_replace('USNM ', '', $v) : $v;
+          // Subjects
+          if($csv_key === 1) {
+            // $this->u->dumper($csv_data[$csv_key]);
+            $json_validation_result['subject'] = (object)$this->repoValidate->validateData($csv_data[$csv_key], 'subject', $blacklisted_fields);
+            // $this->u->dumper($json_validation_result,0);
           }
-          // Convert the array to an object.
-          $json_object[$csv_key][] = (object)$json_array[$key];
-        }
-
-        // $this->u->dumper($json_array);
-        // $this->u->dumper($json_object);
-
-        // Projects
-        if($csv_key === 0) {
-          // $json_validation_result[$csv_key] = (object)$this->repoValidate->validateData($json_object[$csv_key], 'project', $blacklisted_fields);
-          // $this->u->dumper($json_validation_result);
-        }
-        // Subjects
-        if($csv_key === 1) {
-          // $this->u->dumper($json_object[$csv_key]);
-          $json_validation_result['subject'] = (object)$this->repoValidate->validateData($json_object[$csv_key], 'subject', $blacklisted_fields);
-          // $this->u->dumper($json_validation_result,0);
-        }
-        // Items
-        if($csv_key === 2) {
-          // $this->u->dumper($json_object[$csv_key]);
-          $json_validation_result['item'] = (object)$this->repoValidate->validateData($json_object[$csv_key], 'item', $blacklisted_fields);
-          // $this->u->dumper($json_validation_result);
+          // Items
+          if($csv_key === 2) {
+            // $this->u->dumper($csv_data[$csv_key]);
+            $json_validation_result['item'] = (object)$this->repoValidate->validateData($csv_data[$csv_key], 'item', $blacklisted_fields);
+            // $this->u->dumper($json_validation_result);
+          }
         }
 
       }
 
       return $json_validation_result;
-
-      // $response = new JsonResponse($json_validation_result);
-      // return $response;
     }
 
 }
