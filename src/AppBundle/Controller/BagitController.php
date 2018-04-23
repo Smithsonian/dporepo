f990dded9aaf26bb4c255decddaba0777d6dd92c
Goran Halusa <ghalusa@gmail.com>
2018-04-23 11:31:47 -0400

    Fixed and polished-up BagIt logic (DPO3DREP-142)

--- src/AppBundle/Controller/BagitController.php
+++ src/AppBundle/Controller/BagitController.php
@@ -8,8 +8,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Finder\Finder;
 
+// Custom utility bundle
+use AppBundle\Utils\AppUtilities;
+
 class BagitController extends Controller
 {
+  /**
+   * @var object $u
+   */
+  public $u;
 
   /**
    * @var string $bagitPath
@@ -34,8 +41,11 @@ class BagitController extends Controller
   /**
    * Constructor
    */
-  public function __construct()
+  public function __construct(AppUtilities $u)
   {
+    // Usage: $this->u->dumper($variable);
+    $this->u = $u;
+    // Directory paths
     $this->bagitPath = __DIR__ . '/../../../vendor/scholarslab/bagit/lib/bagit.php';
     $this->testBagFiles = __DIR__ . '/../../../web/uploads/testbag_files';
     $this->testBagDestination = __DIR__ . '/../../../web/uploads/testbag';
@@ -44,16 +54,20 @@ class BagitController extends Controller
 
   /**
    * @Route("/admin/bagit", name="bagit-landing-page")
+   *
+   * @return render
    */
-  public function indexAction(Request $request)
+  public function indexAction()
   {
     return $this->render('default/bagit.html.twig');
   }
 
   /**
    * @Route("/admin/bagit/create", name="bagit-create")
+   *
+   * @return redirect
    */
-  public function createAction(Request $request)
+  public function createAction()
   {
     require $this->bagitPath;
 
@@ -80,10 +94,14 @@ class BagitController extends Controller
 
   /**
    * @Route("/admin/bagit/check", name="bagit-check")
+   *
+   * @param object $request  Symfony's request object
+   * @return JsonResponse  The results of the check in JSON format
    */
   public function checkerAction(Request $request)
   {
     $folderpath = false;
+    $checkfiles = [];
 
     // Check to see if the directory exists.
     if(is_dir(__DIR__ . '/../../..' . $request->request->get('folderpath'))) {
@@ -97,36 +115,32 @@ class BagitController extends Controller
     require $this->bagitPath;
     $checkfiles = $this->validatefolder($folderpath);
 
-    if (isset($checkfiles['files']) && count($checkfiles['files']) === 0) {
+    // If all of the BagIt files are present
+    // (bag-info.txt, bagit.txt, manifest-sha1.txt, tagmanifest-sha1.txt),
+    // proceed to validate the payload.
+    if (empty($checkfiles)) {
 
       $bag = new \BagIt($folderpath);
       $validation = $bag->validate();
 
       if (count($validation) > 0) {
         foreach ($validation as $message) {
-          $full_message = '';
-          foreach ($message as $k => $value) {
-            $full_message .= ' ' . $value;
-          }
-          $checkfiles[] = $full_message;
+          $checkfiles[] = implode(': ', $message);
         }
       }
     }
 
-    if(count($checkfiles) > 0) {
-      return new JsonResponse($checkfiles);
-    } else {
-      return new JsonResponse(array());
-    }
-
+    return new JsonResponse($checkfiles);
   }
 
   /**
    * @Route("/admin/bagit/update", name="bagit-update")
+   *
+   * @param object $request Symfony's request object
+   * @return JsonResponse  The results of the check in JSON format
    */
   public function updateAction(Request $request)
   {
-
     $folderpath = false;
 
     // Check to see if the directory exists.
@@ -139,69 +153,32 @@ class BagitController extends Controller
 
     // Otherwise continue...
     require $this->bagitPath;
-    //$fileparts = pathinfo($folderpath);
-
     $bag = new \BagIt($folderpath);
     $bag->update();
 
     return new JsonResponse( array("flag" => true) );
   }
 
-  public function validatefolder($path)
-  {
-
-    $missingFiles = [];
-
-    // Check to see if the data directory exists.
-    if(!is_dir($path . '/data')) {
-      $missingFiles[] = 'data directory';
-    }
-    // Check to see if there are files in the data direcotry.
-    // TODO: Check to see if the total amount of files matches what's in the manifest.
-    if(is_dir($path . '/data')) {
-      $files = array();
-      $finder = new Finder();
-      $finder->files()->in($path . '/data');
-      // Construct an array of files found in the data directory.
-      foreach ($finder as $file) {
-        $files[] = $file->getRealPath();
-      }
-      // If the directory is empty, add it to the missingFiles array.
-      if(empty($files)) $missingFiles[] = 'data directory is empty';
-    }
-    // Check to see if the bag-info.txt is present.
-    if (!file_exists($path . '/bag-info.txt')) $missingFiles[] = 'Baginfo';
-    // Check to see if the manifest-sha1.txt is present.
-    if (!file_exists($path . '/manifest-sha1.txt')) $missingFiles[] = 'Manifest'; //  && !file_exists($path.'/manifest-md5.txt')
-    // Check to see if the bagit.txt is present.
-    if (!file_exists($path . '/bagit.txt')) $missingFiles[] = 'Bagit';
-    // Check to see if the tagmanifest-sha1.txt is present.
-    if (!file_exists($path . '/tagmanifest-sha1.txt')) $missingFiles[] = 'Tag Manifest';
-
-    $missing = array("files" => $missingFiles);
-
-    return $missing;
-  }
-
   /**
-   * Dumper
-   *
-   * For debugging. Outputs data using var_dump(), encapsulated by the <pre> tag, with the option to die() or let it ride.
-   * If an IP address is passed, then only that IP address will be able to view the output.
-   *
-   * @param   mixed   $data         The data value
-   * @param   bool    $die          The data value
-   * @param   string  $ip_address   The data value
-   * @return  mixed   The formatted data
+   * @param int $path  The target directory path
+   * @return array
    */
-  public function dumper($data = false, $die = true, $ip_address=false)
+  public function validatefolder($path = null)
   {
-    if(!$ip_address || $ip_address == $_SERVER["REMOTE_ADDR"]){
-      echo '<pre>';
-      var_dump($data);
-      echo '</pre>';
-      if($die) die();
+    if(!empty($path)) {
+      $missingFiles = [];
+
+      // Check to see if the bag-info.txt is present.
+      if (!file_exists($path . '/bag-info.txt')) $missingFiles[] = 'Baginfo';
+      // Check to see if the manifest-sha1.txt is present.
+      if (!file_exists($path . '/manifest-sha1.txt')) $missingFiles[] = 'Manifest'; //  && !file_exists($path.'/manifest-md5.txt')
+      // Check to see if the bagit.txt is present.
+      if (!file_exists($path . '/bagit.txt')) $missingFiles[] = 'Bagit';
+      // Check to see if the tagmanifest-sha1.txt is present.
+      if (!file_exists($path . '/tagmanifest-sha1.txt')) $missingFiles[] = 'Tag Manifest';
     }
+
+    return $missingFiles;
   }
 
 }
