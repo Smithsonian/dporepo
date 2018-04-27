7fda1662faca726b15c5cac1338876c6d29b0d6d
Goran Halusa <ghalusa@gmail.com>
2018-04-27 19:03:19 -0400

    Added the typeahead-bundle
    DPO3DREP-142
    https://github.com/lifo101/typeahead-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new FR3D\LdapBundle\FR3DLdapBundle(),
+            new Lifo\TypeaheadBundle\LifoTypeaheadBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
