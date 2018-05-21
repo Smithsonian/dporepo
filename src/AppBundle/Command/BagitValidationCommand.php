bcf5fadbeb9f93a2910a0c9c567ea2d51af3771d
Goran Halusa <ghalusa@gmail.com>
2018-05-21 16:58:37 -0400

    Polished-up the BigIt validation console command
    - Insured that the command can be run standalone by providing a 'localpath'
    - Added detailed comments

--- src/AppBundle/Command/BagitValidationCommand.php
+++ src/AppBundle/Command/BagitValidationCommand.php
@@ -12,81 +12,89 @@ use AppBundle\Controller\BagitController;
 
 class BagitValidationCommand extends ContainerAwareCommand
 {
-    private $bagit;
-    protected $container;
+  private $bagit;
+  protected $container;
 
-    public function __construct(BagitController $bagit)
-    {
-        // $this->container = $this->getContainer();
-        $this->bagit = $bagit;
+  public function __construct(BagitController $bagit)
+  {
+    // BagIt Controller.
+    $this->bagit = $bagit;
+    // This is required due to parent constructor, which sets up name.
+    parent::__construct();
+  }
 
-        // this is required due to parent constructor, which sets up name 
-        parent::__construct();
+  protected function configure()
+  {
+    $this
+      // The name of the command (the part after "bin/console").
+      ->setName('app:bagit-validate')
+      // The short description shown while running "php bin/console list".
+      ->setDescription('Validates a bag.')
+      // The full command description shown when running the command with
+      // the "--help" option.
+      ->setHelp('This command allows you to validate a BagIt "bag", which consists of a "payload" (the arbitrary content) and "tags", which are metadata files intended to document the storage and transfer of the "bag".')
+      // Add arguments...
+      ->addArgument('localpath', InputArgument::OPTIONAL, 'The path to the directory to validate.');
+      // ->addArgument('create_data_dir', InputArgument::OPTIONAL, 'Create a data directory? Default: true')
+      // ->addArgument('overwrite_manifest', InputArgument::OPTIONAL, 'Overwrite the manifest? Default: false')
+      // ->addArgument('flag_warnings_as_errors', InputArgument::OPTIONAL, 'Flag warnings as errors. Default: false');
+  }
+
+  /**
+   * Example:
+   * php bin/console app:bagit-validate /var/www/html/dporepo/web/uploads/repository/4
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $result = '';
+    $container = $this->getContainer();
+
+    // Outputs multiple lines to the console (adding "\n" at the end of each line).
+    $output->writeln([
+      '',
+      '<bg=green;options=bold>  BagIt Validator  </>',
+      '<bg=green;options=bold>  ===============  </>',
+      '',
+    ]);
+
+    // If a localpath is passed, use it as the path to the files to validate.
+    if ( !empty($input->getArgument('localpath')) ) {
+      $directory_to_validate = $input->getArgument('localpath');
     }
 
-    protected function configure()
-    {
-
-        $this
-            // the name of the command (the part after "bin/console")
-            ->setName('app:bagit-validate')
-            // the short description shown while running "php bin/console list"
-            ->setDescription('Validates a bag.')
-            // the full command description shown when running the command with
-            // the "--help" option
-            ->setHelp('This command allows you to validate a BagIt "bag", which consists of a "payload" (the arbitrary content) and "tags", which are metadata files intended to document the storage and transfer of the "bag".')
-            // add arguments
-            // ->addArgument('create_data_dir', InputArgument::REQUIRED, 'Create a data directory? Default: true')
-            // ->addArgument('overwrite_manifest', InputArgument::OPTIONAL, 'Overwrite the manifest? Default: false')
-            ->addArgument('flag_warnings_as_errors', InputArgument::OPTIONAL, 'Flag warnings as errors. Default: false');
+    // If a localpath is NOT passed, check the database for a job with the 'job_status' set to 'in progress'.
+    if ( empty($input->getArgument('localpath')) ) {
+      $directory_to_validate = $this->bagit->needs_validation_checker($container);
     }
 
-    /**
-     * Example:
-     * php bin/console app:bagit-validate /var/www/html/dporepo/web/uploads/repository/4 true false false
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $result = '';
-        $container = $this->getContainer();
+    if (!empty($directory_to_validate)) {
 
-        // outputs multiple lines to the console (adding "\n" at the end of each line)
-        $output->writeln([
-            '',
-            '<bg=green;options=bold>  BagIt Validator  </>',
-            '<bg=green;options=bold>  ===============  </>',
-            '',
-        ]);
+      // Parameters to pass to the bagit_validate method.
+      $params = array(
+        'localpath' => $directory_to_validate,
+        'flag_warnings_as_errors' => false,
+      );
 
-        $directory_to_validate = $this->bagit->needs_validation_checker($container);
+      // Run the validation.
+      // If no BagIt-based files are found, then they will be generated.
+      $result = $this->bagit->bagit_validate($params, $container);
 
-        if(!empty($directory_to_validate)) {
+      // Output validation results.
+      if (isset($result['result'])) {
+        $output->writeln('<comment>Validation Result: ' . $result['result'] . "</comment>\n");
+      }
 
-          $params = array(
-            'localpath' => $directory_to_validate,
-            // 'create_data_dir' => false,
-            // 'overwrite_manifest' => false,
-            'flag_warnings_as_errors' => false,
-          );
-
-          $result = $this->bagit->bagit_validate($params, $container);
-
-          if(isset($result['result'])) {
-            $output->writeln('<comment>Validation Result: ' . $result['result'] . "</comment>\n");
-          }
-
-          if(isset($result['errors'])) {
-            foreach ($result['errors'] as $key => $value) {
-              $output->writeln('<error>' . $value . '</error>');
-            }
-          }
-          // var_dump($result); die();
+      // Output errors.
+      if (isset($result['errors'])) {
+        foreach ($result['errors'] as $key => $value) {
+          $output->writeln('<error>' . $value . '</error>');
         }
-
-        // retrieve the argument value using getArgument()
-        // $output->writeln('create_data_dir: ' . $input->getArgument('create_data_dir'));
-        // $output->writeln('overwrite_manifest: ' . $input->getArgument('overwrite_manifest'));
-        // $output->writeln('flag_warnings_as_errors: ' . $input->getArgument('flag_warnings_as_errors'));
-        // $output->writeln('NEEDS TO BE VALIDATED: ' . $directory_to_validate);        
+      }
     }
+
+    // If there's no $directory_to_validate, display a message.
+    if(empty($directory_to_validate)) {
+      $output->writeln('<comment>No jobs found to validate</comment>');
+    }   
+  }
 }
