04a96862401adc9cf3081a24e3a562556ac23a8b
Goran Halusa <ghalusa@gmail.com>
2018-06-11 18:29:21 -0400

    Convert the CSV's boolean-based fields to boolean

--- src/AppBundle/Service/RepoValidateData.php
+++ src/AppBundle/Service/RepoValidateData.php
@@ -51,6 +51,8 @@ class RepoValidateData implements RepoValidate {
     // If data is passed, go ahead and process.
     if(!empty($data)) {
 
+      $integer_field_types = $boolean_field_types = array();
+
       $jsonSchemaObject = json_decode(file_get_contents($this->schema_dir . $schema_definitions_dir . $schema . '.json'));
 
       // TEMPORARY: Keep conditional and var_dump() here for debugging during development.
@@ -58,21 +60,34 @@ class RepoValidateData implements RepoValidate {
       // if($schema === 'capture_dataset') {
 
         // Convert the CSV's integer-based fields to integers... 
+        // Convert the CSV's boolean-based fields to booleans... 
         // because out of the box, all of the CSV's array values are strings.
 
-        // First, reference the JSON schema to gather all of the field names with the type of integer.
+        // First, reference the JSON schema to gather:
+        // 1) all of the field names with the type of integer.
+        // 2) all of the field names with the type of boolean.
         $schema_properties = $jsonSchemaObject->items->properties;
         foreach ($schema_properties as $key => $value) {
+          // Integer field types
           if(isset($value->type) && ($value->type === 'integer')) {
             $integer_field_types[] = $key;
           }
+          // Boolean field types
+          if(isset($value->type) && ($value->type === 'boolean')) {
+            $boolean_field_types[] = $key;
+          }
         }
 
-        // Convert field values from strings to integers.
+        // Convert field values from string to integer or boolean.
         foreach ($data as $data_key => $data_value) {
           foreach ($data_value as $dv_key => $dv_value) {
-            if(in_array($dv_key, $integer_field_types)) {
+            // Cast to integer
+            if(!empty($integer_field_types) && in_array($dv_key, $integer_field_types)) {
               $data[$data_key]->$dv_key = (int)$dv_value;
+            }
+            // Cast to boolean
+            if(!empty($boolean_field_types) && in_array($dv_key, $boolean_field_types)) {
+              $data[$data_key]->$dv_key = (bool)$dv_value;
             }
           }
         }
