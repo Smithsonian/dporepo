e5b63bf2b6134a6a5aea5c4e069935f112c7ab80
Goran Halusa <ghalusa@gmail.com>
2018-07-07 09:00:23 -0400

    get_directory_contents now supports all scenarios
    - Upload details page
    - Capture Dataset form page
    - Capture Dataset Elements browse page

--- src/AppBundle/Controller/FilesystemHelperController.php
+++ src/AppBundle/Controller/FilesystemHelperController.php
@@ -57,82 +57,28 @@ class FilesystemHelperController extends Controller
    *
    * Get Directory Contents
    *
+   * Examples:
+   * http://127.0.0.1:8000/admin/get_directory_contents/2&id=#
+   * http://127.0.0.1:8000/admin/get_directory_contents/false?path=/testupload05/data/&record_id=2&id=#
+   *
    * @param object  $request  Request object
    * @return string
    */
   public function get_directory_contents(Request $request) {
 
-    $data = [];
-    $job_id = !empty($request->attributes->get('job_id')) ? $request->attributes->get('job_id') : false;
-    $id = (!empty($request->get('id')) && ($request->get('id') !== '#')) ? DIRECTORY_SEPARATOR . $request->get('id') : '';
-    $directory_opened_state = (!empty($request->get('id')) && ($request->get('id') === '#')) ? true : false;
+    $data = $job_data = [];
+
+    // The uploads directory path.
     $project_dir = $this->get('kernel')->getProjectDir() . $this->uploads_directory;
-
-    if (!empty($job_id) && is_dir($project_dir . $job_id . $id . DIRECTORY_SEPARATOR)) {
-
-      $finder = new Finder();
-      $finder->in($project_dir . $job_id . $id . DIRECTORY_SEPARATOR);
-      $finder->depth('== 0');
-      $finder->sortByName();
-
-      foreach ($finder as $file) {
-
-        $this_file_path = str_replace($project_dir, '', $file->getPathname());
-        $this_file_path = $this->browser_path . $this_file_path;
-        $this_file_path_array = explode(DIRECTORY_SEPARATOR, $this_file_path);
-        $this_file_name = array_pop($this_file_path_array);
-        $this_file_id = str_replace($project_dir . $job_id . DIRECTORY_SEPARATOR, '', $file->getPathname());
-
-        // $this->u->dumper($project_dir,0);
-        // $this->u->dumper($this_file_id,0);
-        // $this->u->dumper($this_file_name);
-
-        if(is_dir($file->getPathname())) {
-          $data[] = array(
-            'text' => $this_file_name,
-            'children' => true,
-            'id' => $this_file_id,
-            'icon' => 'glyphicon glyphicon-folder-close',
-            'size' => filesize($file->getPathname()),
-            'state' => array('opened' => $directory_opened_state, 'disabled' => false)
-          );
-        }
-        else {
-          $data[] = array(
-            'text' => $this_file_name,
-            'children' => false,
-            'id' => $this_file_id,
-            'type' => 'file',
-            'icon' => 'glyphicon glyphicon-file',
-            'a_attr' => array('href' => $this_file_path, 'download' => $this_file_name)
-          );
-        }
-      }
-    }
-
-    return $this->json($data);
-  }
-
-  /**
-   * @Route("/admin/get_uploaded_files", name="get_uploaded_files", methods="GET")
-   *
-   * Get Uploaded Files
-   * Construct a directory listing of all uploaded files.
-   * Example: /admin/get_uploaded_files?path=/testupload05/data/&id=2
-   *
-   * @param object  $request  Request object
-   * @return string
-   */
-  public function get_uploaded_files(Request $request) {
-
-    $items = array();
-    $directory_path = !empty($request->get('path')) ? $request->get('path') : false;
-    $record_id = !empty($request->get('id')) ? $request->get('id') : false;
+    // URL attributes
+    $job_id = (!empty($request->attributes->get('job_id')) && ($request->attributes->get('job_id') !== 'false')) ? $request->attributes->get('job_id') : false;
+    $id = (!empty($request->get('id')) && ($request->get('id') !== '#')) ? DIRECTORY_SEPARATOR . $request->get('id') : '';
+    $directory_path = !empty($request->get('path')) ? $request->get('path') : '';
+    $record_id = !empty($request->get('record_id')) ? $request->get('record_id') : false;
 
     if(!empty($directory_path) && !empty($record_id)) {
-
+      // Set the container.
       $this->repo_storage_controller->setContainer($this->container);
-
       // Get the job ID so it can be added to the uploaded files path.
       $job_data = $this->repo_storage_controller->execute('getRecords', array(
         'base_table' => 'job_import_record',
@@ -146,76 +92,63 @@ class FilesystemHelperController extends Controller
         'omit_active_field' => true,
         )
       );
-
-      // If data is returned, proceed with parsing a list of uploaded files.
-      if ($job_data && !empty($job_data)) {
-        // The target directory.
-        $dir = $this->get('kernel')->getProjectDir() . DIRECTORY_SEPARATOR . 'web' . $this->browser_path . DIRECTORY_SEPARATOR . $job_data[0]['job_id'] . str_replace('/', DIRECTORY_SEPARATOR, $directory_path);
-
-        // The target directory.
-        // $dir = $this->get('kernel')->getProjectDir() . DIRECTORY_SEPARATOR . 'web' . $this->browser_path . DIRECTORY_SEPARATOR . $job_data[0]['job_id'];
-
-        // Construct the JSON for the tree.
-        // $data = $this->u->get_directory_tree($dir);
-        $data = $this->get_tree($dir);
-
-        // $this->u->dumper($data);
-      }
-
     }
 
-    return $this->json($data);
-  }
+    // Overwrite the $job_id if pulling data from the 'job_import_record' table.
+    $job_id = ($job_data && !empty($job_data)) ? $job_data[0]['job_id'] : $job_id;
+    // Set the directory opened state.
+    $directory_opened_state = (empty($job_data) && !empty($request->get('id')) && ($request->get('id') === '#')) ? true : false;
 
-  public function get_tree($dir = null, $project_dir = null, $previous_file_paths = array(), $previous_dirs = array()) {
-
-    // $this->u->dumper('previous_file_paths',0);
-    // $this->u->dumper($previous_file_paths,0);
-
-    $items = array();
-
-    if (empty($project_dir)) {
-      $project_dir = $this->get('kernel')->getProjectDir();
+    if ($job_data && !empty($job_data) && empty($id)) {
+      $target_directory = $project_dir . $job_id . $directory_path . DIRECTORY_SEPARATOR;
+    } else {
+      $target_directory = $project_dir . $job_id . $id . DIRECTORY_SEPARATOR;
     }
 
-    if(!empty($dir) && is_dir($dir)) {
+    if (!empty($job_id) && is_dir($target_directory)) {
 
       $finder = new Finder();
-      $finder->in($dir);
+      $finder->in($target_directory);
+      $finder->depth('== 0');
       $finder->sortByName();
 
       foreach ($finder as $file) {
 
-        $item = array();
-        $this_file_path = str_replace($project_dir . DIRECTORY_SEPARATOR . 'web', '', $file->getPathname());
-        $this_pretty_file_path = str_replace($project_dir . DIRECTORY_SEPARATOR . 'web' . $this->browser_path . DIRECTORY_SEPARATOR, '', $file->getPathname());
-        $this_pretty_file_path_array = explode(DIRECTORY_SEPARATOR, $this_pretty_file_path);
-        $this_file_name = array_pop($this_pretty_file_path_array);
+        $this_file_path = str_replace($project_dir, '', $file->getPathname());
+        $this_file_path = $this->browser_path . $this_file_path;
+        $this_file_path_array = explode(DIRECTORY_SEPARATOR, $this_file_path);
+        $this_file_name = array_pop($this_file_path_array);
+        $this_file_id = str_replace($project_dir . $job_id . DIRECTORY_SEPARATOR, '', $file->getPathname());
 
-        // If FILE
-        if(!is_dir($file->getPathname()) && !in_array($file->getPathname(), $previous_file_paths)) {
-          $file_entry = array('text' => $this_file_name . '&nbsp;&nbsp;&nbsp;(' . $this->byteConvert(filesize($file->getPathname())) . ')', 'icon' => 'glyphicon glyphicon-file', 'a_attr' => array('href' => $this_file_path, 'path' => $this_pretty_file_path, 'size' => filesize($file->getPathname()), 'download' => $this_file_name));
-          $item = $file_entry;
-          array_push($previous_file_paths, $file->getPathname());
-        }
+        // $this->u->dumper($project_dir,0);
+        // $this->u->dumper($this_file_id,0);
+        // $this->u->dumper(is_dir($file->getPathname()),0);
+        // $this->u->dumper($directory_opened_state);
 
-        // If FOLDER, then go inside and repeat the loop
-        if(is_dir($file->getPathname()) && !in_array($file->getPathname(), $previous_file_paths)) {
-          $folder = array('text' => $this_file_name, 'icon' => 'glyphicon glyphicon-folder-close', 'path' => $this_pretty_file_path, 'size' => filesize($file->getPathname()));
-          $children = $this->get_tree($file->getPathname(), $project_dir, $previous_file_paths, $previous_dirs);
-          $folder['children'] = $children;
-          $item = $folder;
-          array_push($previous_file_paths, $file->getPathname());
+        if(is_dir($file->getPathname())) {
+          $data[] = array(
+            'text' => $this_file_name,
+            'children' => true,
+            'id' => $this_file_id,
+            'icon' => 'glyphicon glyphicon-folder-close',
+            'size' => filesize($file->getPathname()),
+            'state' => array('opened' => $directory_opened_state, 'disabled' => false)
+          );
         }
-        
-        if (!empty($item)) {
-          $items[] = $item;
+        else {
+          $data[] = array(
+            'text' => $this_file_name . '&nbsp;&nbsp;&nbsp;(' . $this->byteConvert(filesize($file->getPathname())) . ')',
+            'children' => false,
+            'id' => $this_file_id,
+            'type' => 'file',
+            'icon' => 'glyphicon glyphicon-file',
+            'a_attr' => array('href' => $this_file_path, 'download' => $this_file_name)
+          );
         }
-
       }
     }
 
-    return $items;
+    return $this->json($data);
   }
 
   /**
@@ -232,7 +165,7 @@ class FilesystemHelperController extends Controller
     $s = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
     $e = floor(log($bytes, 1024));
 
-    return round($bytes/pow(1024, $e), 2).$s[$e];
+    return round($bytes/pow(1024, $e), 2) . ' ' . $s[$e];
   }
 
 }
