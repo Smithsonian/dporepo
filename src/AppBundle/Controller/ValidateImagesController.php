e39df3fa64e25fdddfabe2849bda71cc0b4d6e99
Goran Halusa <ghalusa@gmail.com>
2018-09-19 16:08:49 -0400

    Check if job directory exists and improve error handling

--- src/AppBundle/Controller/ValidateImagesController.php
+++ src/AppBundle/Controller/ValidateImagesController.php
@@ -100,80 +100,86 @@ class ValidateImagesController extends Controller
     $localpath = !empty($params['localpath']) ? $params['localpath'] : false;
 
     // Throw an exception if the job record doesn't exist.
-    if (!$localpath) throw $this->createNotFoundException('The Job directory doesn\'t exist');
+    if (!$localpath) $data[0]['errors'][] = 'Job directory missing from parameters. Please provide a job directory path.';
+    // Set an error if the job record doesn't exist.
+    if (!is_dir($localpath)) $data[0]['errors'][] = 'The Job directory doesn\'t exist';
 
-    // Get the job ID, so errors can be logged to the database.
-    $dir_array = explode(DIRECTORY_SEPARATOR, $localpath);
-    $job_id = array_pop($dir_array);
+    // If the job directory exists, proceed with validating images.
+    if (is_dir($localpath)) {
+      // Get the job ID, so errors can be logged to the database.
+      $dir_array = explode(DIRECTORY_SEPARATOR, $localpath);
+      $job_id = array_pop($dir_array);
 
-    // Search for the data directory.
-    $finder = new Finder();
-    $finder->path('data')->name('/\.jpg|\.tif$/');
-    $finder->in($localpath);
+      // Search for the data directory.
+      $finder = new Finder();
+      $finder->path('data')->name('/\.jpg|\.tif$/');
+      $finder->in($localpath);
 
-    $i = 0;
-    foreach ($finder as $file) {
+      $i = 0;
+      foreach ($finder as $file) {
 
-      if (!$file->isFile()) $data[$i]['errors'][] = 'File is not a valid image.';
+        if (!$file->isFile()) $data[$i]['errors'][] = 'File is not a valid image.';
 
-      if ($file->isFile()) {
+        if ($file->isFile()) {
 
-        $data[$i]['errors'] = array();
+          $data[$i]['errors'] = array();
 
-        // $file->getMTime() — Gets the last modified time
-        // $this->u->dumper($file->getMTime());
+          // $file->getMTime() — Gets the last modified time
+          // $this->u->dumper($file->getMTime());
 
-        if($file->getSize() === 0) {
-          $data[$i]['errors'][] = 'Zero byte file. No further checks will be performed - ' . $file->getFilename();
-          continue;
-        }
-
-        // Validate the mime type.
-        $mime_type = $this->get_mime_type($file->getPathname());
-
-        // Check if this is a valid image according to our hard-coded arrays above.
-        if(array_key_exists($mime_type, $this->valid_image_mimetypes)) {
-          // Check if the image file extensions matches the actual mime type.
-          $mime_type_for_extension = $this->valid_image_types[strtolower($file->getExtension())];
-          if($mime_type_for_extension !== $mime_type) {
-            $data[$i]['errors'][] = 'File extension does not match the image type - ' . $file->getFilename();
+          if($file->getSize() === 0) {
+            $data[$i]['errors'][] = 'Zero byte file. No further checks will be performed - ' . $file->getFilename();
+            continue;
           }
-        } else {
-          $data[$i]['errors'][] = 'File is not a valid image - ' . $file->getFilename();
+
+          // Validate the mime type.
+          $mime_type = $this->get_mime_type($file->getPathname());
+
+          // Check if this is a valid image according to our hard-coded arrays above.
+          if(array_key_exists($mime_type, $this->valid_image_mimetypes)) {
+            // Check if the image file extensions matches the actual mime type.
+            $mime_type_for_extension = $this->valid_image_types[strtolower($file->getExtension())];
+            if($mime_type_for_extension !== $mime_type) {
+              $data[$i]['errors'][] = 'File extension does not match the image type - ' . $file->getFilename();
+            }
+          } else {
+            $data[$i]['errors'][] = 'File is not a valid image - ' . $file->getFilename();
+          }
+
         }
 
+        $data[$i]['file_name'] = $file->getFilename();
+        $data[$i]['file_size'] = $file->getSize();
+        $data[$i]['file_extension'] = $file->getExtension();
+        $data[$i]['file_mime_type'] = $this->get_mime_type($file->getPathname());
+
+        if (!empty($data[$i]['errors'])) {
+          // Set the job_status to 'failed', if not already set.
+          if ($job_status !== 'failed') $job_status = 'failed';
+          // Log the errors to the database.
+          $this->repoValidate->logErrors(
+            array(
+              'job_id' => $job_id,
+              'user_id' => 0,
+              'job_log_label' => 'Image Validation',
+              'errors' => $data[$i]['errors'],
+            )
+          );
+        }
+
+        $i++;
       }
 
-      $data[$i]['file_name'] = $file->getFilename();
-      $data[$i]['file_size'] = $file->getSize();
-      $data[$i]['file_extension'] = $file->getExtension();
-      $data[$i]['file_mime_type'] = $this->get_mime_type($file->getPathname());
-
-      if (!empty($data[$i]['errors'])) {
-        // Set the job_status to 'failed', if not already set.
-        if ($job_status !== 'failed') $job_status = 'failed';
-        // Log the errors to the database.
-        $this->repoValidate->logErrors(
-          array(
-            'job_id' => $job_id,
-            'user_id' => 0,
-            'job_log_label' => 'Image Validation',
-            'errors' => $data[$i]['errors'],
-          )
-        );
-      }
-
-      $i++;
+      // Update the 'job_status' in the 'job' table accordingly.
+      $this->repo_storage_controller->execute('setJobStatus', 
+        array(
+          'job_id' => $job_id, 
+          'status' => $job_status, 
+          'date_completed' => date('Y-m-d h:i:s')
+        )
+      );
     }
 
-    // Update the 'job_status' in the 'job' table accordingly.
-    $this->repo_storage_controller->execute('setJobStatus', 
-      array(
-        'job_id' => $job_id, 
-        'status' => $job_status, 
-        'date_completed' => date('Y-m-d h:i:s')
-      )
-    );
 
     return $data;
   }
