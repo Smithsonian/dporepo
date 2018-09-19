7c546dfecff37f806800eb73667f0e0936b96a04
Goran Halusa <ghalusa@gmail.com>
2018-09-19 18:49:42 -0400

    Initial logic for validating image pairs (cr2 support only)

--- src/AppBundle/Controller/ValidateImagesController.php
+++ src/AppBundle/Controller/ValidateImagesController.php
@@ -75,6 +75,7 @@ class ValidateImagesController extends Controller
       'tiff' => image_type_to_mime_type(IMAGETYPE_TIFF_MM),
       'jpg' => image_type_to_mime_type(IMAGETYPE_JPEG),
       'jpeg' => image_type_to_mime_type(IMAGETYPE_JPEG),
+      'cr2' => image_type_to_mime_type(IMAGETYPE_JPEG),
     );
     // Valid image mime types.
     $this->valid_image_mimetypes = array(
@@ -99,89 +100,161 @@ class ValidateImagesController extends Controller
     $job_status = 'metadata ingest in progress';
     $localpath = !empty($params['localpath']) ? $params['localpath'] : false;
 
-    // Throw an exception if the job record doesn't exist.
+    // Set an error if the job directory path parameter doesn't exist.
     if (!$localpath) $data[0]['errors'][] = 'Job directory missing from parameters. Please provide a job directory path.';
-    // Set an error if the job record doesn't exist.
+    // Set an error if the job directory doesn't exist.
     if (!is_dir($localpath)) $data[0]['errors'][] = 'The Job directory doesn\'t exist';
 
     // If the job directory exists, proceed with validating images.
     if (is_dir($localpath)) {
       // Get the job ID, so errors can be logged to the database.
       $dir_array = explode(DIRECTORY_SEPARATOR, $localpath);
-      $job_id = array_pop($dir_array);
+      $uuid = array_pop($dir_array);
+      $job_data = $this->repo_storage_controller->execute('getJobData', array($uuid));
+
+      // Set an error if the job record doesn't exist.
+      if (empty($job_data)) {
+        $data[0]['errors'][] = 'The job record doesn\'t exist in the database. UUID: ' . $uuid;
+        return $data;
+      }
 
       // Search for the data directory.
       $finder = new Finder();
-      $finder->path('data')->name('/\.jpg|\.tif$/');
+      $finder->path('data')->name('/\.jpg|\.tif|\.cr2$/');
       $finder->in($localpath);
 
       $i = 0;
       foreach ($finder as $file) {
 
-        if (!$file->isFile()) $data[$i]['errors'][] = 'File is not a valid image.';
+        if (!$file->isDir()) {
 
-        if ($file->isFile()) {
+          if (!$file->isFile()) $data[$i]['errors'][] = 'File is not a valid image.';
 
-          $data[$i]['errors'] = array();
+          if ($file->isFile()) {
 
-          // $file->getMTime() — Gets the last modified time
-          // $this->u->dumper($file->getMTime());
+            $data[$i]['errors'] = array();
 
-          if($file->getSize() === 0) {
-            $data[$i]['errors'][] = 'Zero byte file. No further checks will be performed - ' . $file->getFilename();
-            continue;
-          }
+            // $file->getMTime() — Gets the last modified time
+            // $this->u->dumper($file->getMTime());
 
-          // Validate the mime type.
-          $mime_type = $this->get_mime_type($file->getPathname());
-
-          // Check if this is a valid image according to our hard-coded arrays above.
-          if(array_key_exists($mime_type, $this->valid_image_mimetypes)) {
-            // Check if the image file extensions matches the actual mime type.
-            $mime_type_for_extension = $this->valid_image_types[strtolower($file->getExtension())];
-            if($mime_type_for_extension !== $mime_type) {
-              $data[$i]['errors'][] = 'File extension does not match the image type - ' . $file->getFilename();
+            if($file->getSize() === 0) {
+              $data[$i]['errors'][] = 'Zero byte file. No further checks will be performed - ' . $file->getFilename();
+              continue;
             }
-          } else {
-            $data[$i]['errors'][] = 'File is not a valid image - ' . $file->getFilename();
+
+            // Validate the mime type.
+            $mime_type = $this->get_mime_type($file->getPathname());
+
+            // $this->u->dumper($mime_type,0);
+
+            // Check if this is a valid image according to our hard-coded arrays above.
+            if(array_key_exists($mime_type, $this->valid_image_mimetypes)) {
+              // Check if the image file extensions matches the actual mime type.
+              $mime_type_for_extension = $this->valid_image_types[strtolower($file->getExtension())];
+              if($mime_type_for_extension !== $mime_type) {
+                $data[$i]['errors'][] = 'File extension does not match the image type - ' . $file->getFilename();
+              }
+            } else {
+              $data[$i]['errors'][] = 'File is not a valid image - ' . $file->getFilename();
+            }
+
           }
 
+          $data[$i]['file_name'] = $file->getFilename();
+          $data[$i]['file_size'] = $file->getSize();
+          $data[$i]['file_extension'] = $file->getExtension();
+          $data[$i]['file_mime_type'] = $this->get_mime_type($file->getPathname());
+
+          if (!empty($data[$i]['errors'])) {
+            // Set the job_status to 'failed', if not already set.
+            if ($job_status !== 'failed') $job_status = 'failed';
+            // Log the errors to the database.
+            $this->repoValidate->logErrors(
+              array(
+                'job_id' => $job_data['job_id'],
+                'user_id' => 0,
+                'job_log_label' => 'Image Validation',
+                'errors' => $data[$i]['errors'],
+              )
+            );
+          }
+
+          $i++;
         }
+      }
 
-        $data[$i]['file_name'] = $file->getFilename();
-        $data[$i]['file_size'] = $file->getSize();
-        $data[$i]['file_extension'] = $file->getExtension();
-        $data[$i]['file_mime_type'] = $this->get_mime_type($file->getPathname());
-
-        if (!empty($data[$i]['errors'])) {
-          // Set the job_status to 'failed', if not already set.
-          if ($job_status !== 'failed') $job_status = 'failed';
-          // Log the errors to the database.
-          $this->repoValidate->logErrors(
-            array(
-              'job_id' => $job_id,
-              'user_id' => 0,
-              'job_log_label' => 'Image Validation',
-              'errors' => $data[$i]['errors'],
-            )
-          );
+      // If the job hasn't failed, validate image pairs.
+      if ($job_status !== 'failed') {
+        // Run the image pairs validation.
+        $result = $this->validateImagePairs($data, $job_status);
+        // Log the errors to the database.
+        if (!empty($result)) {
+          // Set the job_status to failed.
+          $job_status = 'failed';
+          foreach ($result as $rkey => $rvalue) {
+            $this->repoValidate->logErrors(
+              array(
+                'job_id' => $job_data['job_id'],
+                'user_id' => 0,
+                'job_log_label' => 'Image Pairs Validation',
+                'errors' => $rvalue,
+              )
+            );
+          }
         }
-
-        $i++;
       }
 
       // Update the 'job_status' in the 'job' table accordingly.
-      $this->repo_storage_controller->execute('setJobStatus', 
+      $res = $this->repo_storage_controller->execute('setJobStatus', 
         array(
-          'job_id' => $job_id, 
+          'job_id' => $job_data['uuid'], 
           'status' => $job_status, 
           'date_completed' => date('Y-m-d h:i:s')
         )
       );
+
     }
 
-
     return $data;
+  }
+
+  /**
+   * Validate Image Pairs
+   * @param array $data The data to validate.
+   * @return array containing success/fail value, and any messages.
+   */
+  public function validateImagePairs($data = array(), $job_status = '') {
+
+    $return = array();
+
+    // If no data is passed, set a message.
+    if (empty($data)) $return['messages'][] = 'No image pairs to validate. Please provide an array of files to validate.';
+
+    // If data is passed, go ahead and perform the validation.
+    if (!empty($data)) {
+
+      // Create an array of all files, with the file names as the keys, and the extensions as the values.
+      $all_files = array();
+      foreach($data as $key => $value) {
+        $all_files[$value['file_name']] = $value['file_extension'];
+      }
+
+      // Validate for image pairs if .cr2 files exist.
+      if (count($all_files)) {
+        foreach($all_files as $fkey => $fvalue) {
+          if ($fvalue === 'cr2') {
+            $file_basename = pathinfo($fkey, PATHINFO_FILENAME);
+            // Set an error if a corresponding jpg doesn't exist.
+            if (!array_key_exists($file_basename . '.jpg', $all_files)) {
+              $return[]['errors'] = 'Corresponding JPG not found for ' . $fvalue . ': ' . $fkey;
+            }
+          }
+        }
+      }
+
+    }
+
+    return $return;
   }
 
   /**
