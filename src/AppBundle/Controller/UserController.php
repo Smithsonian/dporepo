11375b64c9b07cbc184eb02695be30759346d4fd
rbartlettquotient <rbartlett@quotient-inc.com>
2018-10-04 05:45:16 -0400

    Create initial routes for getting users and roles

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -38,30 +38,52 @@ class UserController extends Controller {
   }
 
   /**
-   * @Route("/admin/users/roles/", name="roles_list", methods="GET")
+   * @Route("/admin/datatables_browse_roles/", name="datatables_browse_roles", methods={"GET","POST"})
    */
-  public function showRoles(Connection $conn, Request $request) {
+  public function datatables_browse_roles(Request $request) {
 
-    $data = $this->repo_storage_controller->execute('getRecords', array(
-        'base_table' => 'role',
-        'sort_fields' => array(
-          0 => array('field_name' => 'role_title')
-        ),
-      )
+    $req = $request->request->all();
+
+    $search = !empty($req['search']['value']) ? $req['search']['value'] : false;
+    $sort_field = array_key_exists('order', $req) ? $req['columns'][ $req['order'][0]['column'] ]['data'] : 'role_slug';
+    $sort_order = array_key_exists('order', $req) ? $req['order'][0]['dir'] : '';
+    $start_record = !empty($req['start']) ? $req['start'] : 0;
+    $stop_record = !empty($req['length']) ? $req['length'] : 20;
+
+    $query_params = array(
+      'sort_field' => $sort_field,
+      'sort_order' => $sort_order,
+      'start_record' => $start_record,
+      'stop_record' => $stop_record,
     );
+    if ($search) {
+      $query_params['search_value'] = $search;
+    }
+
+    $data = $this->repo_storage_controller->execute('getDatatableRoles', $query_params);
+
+    foreach($data['aaData'] as $k => $r) {
+      $data['aaData'][$k]['permissions'] = str_replace(',', ', ', $r['permissions']);
+    }
+    return $this->json($data);
+
+  }
+
+  /**
+   * @Route("/admin/roles/", name="roles_list", methods={"GET","POST"})
+   */
+  public function showRoles(Request $request) {
 
     return $this->render('users/browse_roles.html.twig', array(
       'page_title' => 'User Roles',
-      'role_data' => $data,
     ));
   }
 
   /**
-   * Matches /admin/users/role_edit/*
    *
    * @Route("/admin/users/role_edit/{role_slug}", name="role_edit", methods={"GET","POST"}, defaults={"role_slug" = null})
    *
-   * @param   int     $role_slug           The dataset_type ID
+   * @param   string  $role_slug    The slug/shortname for the role
    * @param   object  Request       Request object
    * @return  array|bool            The query result
    */
@@ -82,20 +104,85 @@ class UserController extends Controller {
   }
 
   /**
-   * @Route("/admin/users/", name="users_list", methods="GET")
+   * @Route("/admin/datatables_browse_users/", name="datatables_browse_users", methods={"GET","POST"})
    */
-  public function showUsers(Connection $conn, Request $request) {
+  public function datatables_browse_users(Request $request) {
 
-    $data = $this->repo_storage_controller->execute('getRecords', array(
-        'base_table' => 'user',
-        'sort_fields' => array(
-          0 => array('field_name' => 'username')
-        ),
-      )
+    $req = $request->request->all();
+
+    $search = !empty($req['search']['value']) ? $req['search']['value'] : false;
+    $sort_field = array_key_exists('order', $req) ? $req['columns'][ $req['order'][0]['column'] ]['data'] : 'username';
+    $sort_order = array_key_exists('order', $req) ? $req['order'][0]['dir'] : '';
+    $start_record = !empty($req['start']) ? $req['start'] : 0;
+    $stop_record = !empty($req['length']) ? $req['length'] : 20;
+
+    $query_params = array(
+      'sort_field' => $sort_field,
+      'sort_order' => $sort_order,
+      'start_record' => $start_record,
+      'stop_record' => $stop_record,
     );
+    if ($search) {
+      $query_params['search_value'] = $search;
+    }
+
+    $data = $this->repo_storage_controller->execute('getDatatableUsers', $query_params);
+
+    foreach($data['aaData'] as $k => $u){
+      $data['aaData'][$k]['project'] = $u['project_name'];
+      $data['aaData'][$k]['stakeholder'] = trim($u['unit_stakeholder_label'] . ' ' . $u['unit_stakeholder_full_name']);
+    }
+
+    return $this->json($data);
+
+  }
+
+  /**
+   * @Route("/admin/users/", name="users_list", methods={"GET","POST"})
+   */
+  public function browse_users(Request $request)
+  {
 
     return $this->render('users/browse_users.html.twig', array(
       'page_title' => 'Users',
+    ));
+
+  }
+
+
+
+  /**
+   * @Route("/admin/users/{username_canonical}/edit", name="user_edit", methods={"GET","POST"}, defaults={"username_canonical" = null})
+   *
+   * @param   string  username_canonical   The canonical username
+   * @param   object  Request           Request object
+   * @return  array|bool                The query result
+   */
+  public function editUser(Request $request, $username_canonical) {
+
+    $data = $this->repo_storage_controller->execute('getUsers', array('test' => 'test'));
+
+    return $this->render('users/edit_user.html.twig', array(
+      'page_title' => 'Edit User',
+      'role_data' => $data,
+    ));
+  }
+
+  /**
+   * @Route("/admin/users/{username_canonical}/route_name/project_id", name="check_user_permission", methods={"GET","POST"} )
+   *
+   * @param   string  username_canonical   The canonical username
+   * @param   int  route_name   The Symfony route name
+   * @param   int  project_id   The project ID
+   * @param   object  Request           Request object
+   * @return  array|bool                The query result
+   */
+  public function userHasPermission(Request $request, $username_canonical, $route_name, $project_id) {
+
+    $data = $this->repo_storage_controller->execute('getUsers', array('test' => 'test'));
+
+    return $this->render('users/edit_user.html.twig', array(
+      'page_title' => 'Edit User',
       'role_data' => $data,
     ));
   }
