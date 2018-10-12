184ece6bad40da6c5189b52795524ee91e951177
Goran Halusa <ghalusa@gmail.com>
2018-10-12 17:50:11 -0400

    Transfer of job details to the metadata storage service (DPO3DREP-151)
    - Processing job details saved to metadata storage
    - All JSON and TXT output saved to metadata storage
    
    TODO
    - Transfer all other assets to the repository's filesystem.
    - Then, if using an external file storage service, transfer assets to the external file storage service.

--- src/AppBundle/Service/RepoModelValidateInterface.php
+++ src/AppBundle/Service/RepoModelValidateInterface.php
@@ -9,11 +9,20 @@ namespace AppBundle\Service;
 interface RepoModelValidateInterface {
 
   /**
-   * @param $target_directory The directory which contains model(s) to be validated and processed.
-   * @param $filesystem Filesystem object (via Flysystem).
+   * @param string $target_directory The directory which contains model(s) to be validated and processed.
+   * @param object $filesystem Filesystem object (via Flysystem).
    * See: https://flysystem.thephpleague.com/docs/usage/filesystem-api/
    * @return array
    */
   public function validate_models(string $target_directory, obj $filesystem);
+
+  /**
+   * @param string $path The directory which contains model(s) to be validated and processed.
+   * @param array $job_data The repository job's data.
+   * @param object $filesystem Filesystem object (via Flysystem).
+   * See: https://flysystem.thephpleague.com/docs/usage/filesystem-api/
+   * @return array
+   */
+  public function run_validate_models(string $path, array $job_data, obj $filesystem);
 
 }
