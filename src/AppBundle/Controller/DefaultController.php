28a24b8cf8881359e6dc4c051b5ef70aafccdb61
Janusz Woyciechowsky <januszewg@gmail.com>
2018-11-06 16:29:21 -0500

    Check if database exist in order to validate template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,18 +5,23 @@ namespace AppBundle\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use AppBundle\Controller\RepoStorageHybridController;
+use Doctrine\DBAL\Driver\Connection;
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request,Connection $conn)
     {
+        $repo_storage_controller = new RepoStorageHybridController($conn);
+        $ret = $repo_storage_controller->build('databaseExist', null);
+        //$ret = $repo_storage_controller->build('installDatabase', null);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        "dbexist"=>$ret]);
     }
 
     /**
