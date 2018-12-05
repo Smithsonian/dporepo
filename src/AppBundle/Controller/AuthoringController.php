d9aeaca4e40abf6ae2048a3203fb3037a6b86955
Goran Halusa <ghalusa@gmail.com>
2018-12-04 21:37:36 -0500

    Finished creating the WebDAV server env (DPO3DREP-451)

--- src/AppBundle/Controller/AuthoringController.php
+++ src/AppBundle/Controller/AuthoringController.php
@@ -124,7 +124,7 @@ class AuthoringController extends Controller
   }
 
   /**
-   * @Route("/webdav{path}", name="webdav", methods={"GET","POST"}, defaults={"path" = null}, requirements={"path"=".+"})
+   * @Route("/webdav{path}", name="webdav", methods={"GET","POST","PUT", "DELETE","MKCOL","PROPFIND"}, defaults={"path" = null}, requirements={"path"=".+"})
    *
    * Author
    *
@@ -132,22 +132,16 @@ class AuthoringController extends Controller
    * @return string
    */
   public function webdav(Request $request) {
-
-    // $req = $request->request->all();
-    // $this->u->dumper($req);
     
-    // If your server is not on your webroot, make sure the following line has the
-    // correct information
+    // If your server is not on your webroot, make sure the following line has the correct information
     $this->server->setBaseUri('/webdav');
 
-    // The lock manager is reponsible for making sure users don't overwrite
-    // each others changes.
+    // The lock manager is reponsible for making sure users don't overwrite each others changes.
     $lockBackend = new DAV\Locks\Backend\File('data/locks');
     $lockPlugin = new DAV\Locks\Plugin($lockBackend);
     $this->server->addPlugin($lockPlugin);
 
-    // This ensures that we get a pretty index in the browser, but it is
-    // optional.
+    // This ensures that we get a pretty index in the browser, but it is optional.
     $this->server->addPlugin(new DAV\Browser\Plugin());
 
     // All we need to do now, is to fire up the server
