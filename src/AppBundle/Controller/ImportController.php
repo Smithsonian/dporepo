ec9e93428af38623590e9026fdb6c63146e2456f
rbartlettquotient <rbartlett@quotient-inc.com>
2019-01-23 12:32:12 -0500

    Cherry picking rb-DPO3DREP-489 merge conflict (DPO3DREP-503)
    Commit: 3e9b5dc
    
    Change uuid to ingest_job_uuid

--- src/AppBundle/Controller/ImportController.php
+++ src/AppBundle/Controller/ImportController.php
@@ -737,6 +737,10 @@ class ImportController extends Controller
           )
         );
 
+        $query_params = array('ingest_job_uuid' => $uuid);
+        $workflow_data = $this->repo_storage_controller->execute('getWorkflows', $query_params);
+        $job_data['workflow'] = $workflow_data;
+
       }
 
       return $this->render('import/import_summary_item.html.twig', array(
