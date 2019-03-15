0485db2e758b4a2b8b956e20d56add2ed1f9cbed
Goran Halusa <ghalusa@gmail.com>
2019-03-15 15:36:38 -0400

    Created 3 batch scripts, 1 for each environment
    - Needed to do this due to path differences
      C:\xampp\htdocs\dporepo (production)
      C:\xampp\htdocs\dporepo_test (test)
      C:\xampp\htdocs\dporepo_dev (development)

---
+++ src/BatchScripts/generateModelDev.bat
@@ -0,0 +1,11 @@
+@REM generateModel.bat
+@REM Checks the database to find the next 'web-multi' workflow step which hasn't been executed.
+
+@REM Add the directory containing php.exe to the path
+PATH=%PATH%;C:\PHP\
+
+@REM Change into the root of the Symfony application's directory.
+CD C:\xampp\htdocs\dporepo_dev
+
+@REM Execute php bin/console app:model-generate.
+php bin/console app:model-generate
