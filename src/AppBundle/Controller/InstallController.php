97192f4b0ba398628debaaf056fcf6a4164fb3e3
Rebecca Bartlett <rbartlett@quotient-inc.com>
2019-04-26 05:14:00 -0400

    Add function to create first user programmatically
    And add 'admin' role for first user

--- src/AppBundle/Controller/InstallController.php
+++ src/AppBundle/Controller/InstallController.php
@@ -87,13 +87,61 @@ class InstallController extends Controller
     }
     else {
       if ($parameters) {
-        return $this->repo_storage_structure->$function($parameters);
+        $ret = $this->repo_storage_structure->$function($parameters);
       }else{
-        return $this->repo_storage_structure->$function();
+        $ret = $this->repo_storage_structure->$function();
       }
+
+      if(is_array($ret) && isset($ret['installed']) && $ret['installed'] == 1) {
+        $this->createUserOne();
+      }
+
+      return $ret;
 
     }
     return array('installed' => true);
+
+  }
+
+  public function createUserOne() {
+
+      //@todo get from parameters.yml or prompt user
+      $email = 'test@test.com';
+      $username = 'admin';
+      $password = 'foundation';
+
+      $userManager = $this->get('fos_user.user_manager');
+
+      // Or you can use the doctrine entity manager if you want instead the fosuser manager
+      // to find
+      //$em = $this->getDoctrine()->getManager();
+      //$usersRepository = $em->getRepository("mybundleuserBundle:User");
+      // or use directly the namespace and the name of the class
+      // $usersRepository = $em->getRepository("mybundle\userBundle\Entity\User");
+      //$email_exist = $usersRepository->findOneBy(array('email' => $email));
+
+      //@todo not really needed but...
+      // Check if the user exists to prevent Integrity constraint violation error in the insertion
+      $email_exist = $userManager->findUserByEmail($email);
+      if($email_exist){
+          return false;
+      }
+
+      $user = $userManager->createUser();
+      $user->setUsername($username);
+      $user->setEmail($email);
+      $user->setEmailCanonical($email);
+      $user->setEnabled(1); // enable the user or enable it later with a confirmation token in the email
+      // this method will encrypt the password with the default settings :)
+      $user->setPlainPassword($password);
+      $userManager->updateUser($user);
+
+      $params = array(
+        'user_id' => 1,
+        'username_canonical' => 'admin',
+        'role_id' => 1
+      );
+      $this->repo_storage_controller->execute('saveUserRole', $params);
 
   }
 
